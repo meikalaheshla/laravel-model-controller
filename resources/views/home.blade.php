f3921269346ffaebd36ce654b3c60503988e793b
meikalaheshla <fausto.boshoff@gmail.com>
2022-09-19 17:51:02 +0200

    correct misstype

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,9 @@
 @section('main-content')
 <div class="container">
     <div class="row">
-        <div class="col"></div>
+        <div class="col">
+            
+        </div>
     </div>
 </div>
 @endsection
