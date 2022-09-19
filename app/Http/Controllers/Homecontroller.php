d7a8418d2578784fd049a19d0b07a982e40e6d1a
meikalaheshla <fausto.boshoff@gmail.com>
2022-09-19 17:46:15 +0200

    linked db with repo

--- app/Http/Controllers/Homecontroller.php
+++ app/Http/Controllers/Homecontroller.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Movie;
 
 class Homecontroller extends Controller
 {
-    public function index(){
-        return view('home');
+    public function index()
+    {
+        $movies = Movie::all();
+
+        return view('home', compact('movies'));
     }
 }
