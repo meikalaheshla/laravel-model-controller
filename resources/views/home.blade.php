1c63431a2f1f60e596e0d708f1318db76590af42
meikalaheshla <fausto.boshoff@gmail.com>
2022-09-19 18:05:12 +0200

    linked css to main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,15 @@
 @section('main-content')
 <div class="container">
     <div class="row">
-        <div class="col">
-            
+        @forelse($movies as $movie)
+        <div class="col-6">
+            <div class="my-card">
+                <h1>{{$movie->title}}</h1>
+            </div>
         </div>
+        @empty
+        <h3>Nessun film disponibile</h3>
+        @endforelse
     </div>
 </div>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     {{--style--}}
-    <link rel="stylesheet" href"{{assert('css/app.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
     {{--CSS--}}
     <script defer src="{{asset('js/app.js')}}"></script>
     
@@ -14,7 +14,7 @@
 <body>
     
     <main>
-        @yeld('main-content')
+        @yield('main-content')
     </main>
         
 </body>
