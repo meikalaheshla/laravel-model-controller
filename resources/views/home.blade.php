11265e83597bec9b15a912cb61d1909a30fd66f3
meikalaheshla <fausto.boshoff@gmail.com>
2022-09-19 18:22:21 +0200

    styled cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,11 @@
 <div class="container">
     <div class="row">
         @forelse($movies as $movie)
-        <div class="col-6">
+        <div class="col-4">
             <div class="my-card">
                 <h1>{{$movie->title}}</h1>
+                <h2>{{$movie->original_title}}</h2>
+                <h3>{{$movie->date}}</h3>
             </div>
         </div>
         @empty
